f8eb7d3d86fd3c1b0e1ca217a5ff1bb5c6e8920a
Mohammed-Raiyan-Ahmed <129960040+Mohammed-Raiyan-Ahmed@users.noreply.github.com>
2023-04-07 08:20:22 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-set -o errexit
-pip install --upgrade pip
-pip install flask
-pip install numpy
-pip install Pillow
-pip install tensorflow
-pip install keras_preprocessing
-pip install -r requirements.txt
