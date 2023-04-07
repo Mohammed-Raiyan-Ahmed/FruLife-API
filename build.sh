d85514162cae20a29b1250d10f6304a3c8befb2b
Mohammed-Raiyan-Ahmed <129960040+Mohammed-Raiyan-Ahmed@users.noreply.github.com>
2023-04-07 08:03:54 +0530

    configuration for deploying on render

---
+++ build.sh
@@ -0,0 +1,8 @@
+set -o errexit
+pip install --upgrade pip
+pip install flask
+pip install numpy
+pip install Pillow
+pip install tensorflow
+pip install keras_preprocessing
+pip install -r requirements.txt
